8ab296306b594fddfa09d2db5f868bcd969d83cb
DenisSoloviev <dionisio14solo14@gmail.com>
2019-01-04 18:03:25 +0200

    full main page without bottom footer and popup's

--- app/js/common.js
+++ app/js/common.js
@@ -2,4 +2,15 @@ $(function() {
 	$('.home-slider').slick({
 		dots: true
 	});
+	$('.product__slider').slick({
+		prevArrow: $('.product .arrows .prev'),
+		nextArrow: $('.product .arrows .next')
+	});
+	$('.plumbing__slider').slick({
+		prevArrow: $('.plumbing .caption-row .arrows-navigation li.prev'),
+		nextArrow: $('.plumbing .caption-row .arrows-navigation li.next')
+	});
+	$('.ft-slider').slick({
+		slidesToShow: 5
+	});
 });
