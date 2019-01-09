004297248b14b154d9df7c6c542e2853f315e29a
DenisSoloviev <dionisio14solo14@gmail.com>
2019-01-09 18:12:42 +0200

    footer and tabs

--- app/js/common.js
+++ app/js/common.js
@@ -13,4 +13,26 @@ $(function() {
 	$('.ft-slider').slick({
 		slidesToShow: 5
 	});
+	
+	
+	//tabs
+	
+	var $productTabs = $('.product .tabs a'),
+		$slides = $('.product__slider .slick-slide'),
+		$filteredSlides = [];
+	$slides.each(function(i) {
+		if($(this).hasClass('slick-cloned')) {
+		   return;
+		 } else {
+			 $filteredSlides.push($(this));
+		 }
+	});
+	
+	console.log($filteredSlides);
+	$productTabs.click(function(e) {
+		e.preventDefault();
+		$(this).addClass('active').siblings().removeClass('active');
+		console.log($slides);
+	});
+	
 });
