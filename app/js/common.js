7b0da0cf350f3548fee6f4b59961d2c1a8d8b1f8
DenisSoloviev <dionisio14solo14@gmail.com>
2019-01-11 18:04:32 +0200

    tabs, dropdown menu, breadcrumbs beginning

--- app/js/common.js
+++ app/js/common.js
@@ -3,8 +3,8 @@ $(function() {
 		dots: true
 	});
 	$('.product__slider').slick({
-		prevArrow: $('.product .arrows .prev'),
-		nextArrow: $('.product .arrows .next')
+		prevArrow: $('.product .arrows li.prev'),
+		nextArrow: $('.product .arrows li.next')
 	});
 	$('.plumbing__slider').slick({
 		prevArrow: $('.plumbing .caption-row .arrows-navigation li.prev'),
@@ -15,24 +15,75 @@ $(function() {
 	});
 	
 	
-	//tabs
+	// tabs
 	
 	var $productTabs = $('.product .tabs a'),
-		$slides = $('.product__slider .slick-slide'),
-		$filteredSlides = [];
-	$slides.each(function(i) {
-		if($(this).hasClass('slick-cloned')) {
-		   return;
-		 } else {
-			 $filteredSlides.push($(this));
-		 }
-	});
+		$sliders = $('.product__slider');
 	
-	console.log($filteredSlides);
 	$productTabs.click(function(e) {
 		e.preventDefault();
 		$(this).addClass('active').siblings().removeClass('active');
-		console.log($slides);
+		$sliders.removeClass('active').eq($(this).index()).addClass('active');
+		$('.product__slider').slick('setPosition');
+		
+	});
+	
+	// counter for products
+	
+	var $plus = $('.product__slider .card-item .bottom-part .add-to-card .plus');
+	
+	console.log($plus);
+	
+	$('.product__slider .card-item .bottom-part .add-to-card .plus').on('click', function(e) {
+		var $input = $(this).siblings('input'),
+			$inputVal = $input.attr('value');
+		$input.attr('value', parseInt($inputVal) + 1);
+	});
+	
+	$('.product__slider .card-item .bottom-part .add-to-card .minus').on('click', function(e) {
+		var $input = $(this).siblings('input'),
+			$inputVal = $input.attr('value');
+		if($inputVal < 1) {
+			$input.attr('value', parseInt(0));
+		} else {
+			$input.attr('value', parseInt($inputVal) - 1);
+		}
+		
+	});
+	
+	
+	// dropdown category
+	
+	var $categoriesDropdown = $('.header__middle form .dropdown');
+	$categoriesDropdown.click(function() {
+		$(this).find('.search-category').slideToggle();
+	});
+	
+	
+	// hover main menu
+	
+	var $liCategories = $('.header__bottom nav ul li.catalog');
+	var $categoryDropdown = $('.header__bottom nav .category-dropdown');
+	$liCategories.on('click', function() {
+		$categoryDropdown.slideToggle();
+	})
+	
+	
+	// hover plumbing
+	
+	var $plumbingContainer = $('.plumbing__slider .plumbing-slide'),
+		$plumbingBtn = $plumbingContainer.find('.more');
+	
+	$plumbingContainer.hover(function() {
+		$(this).find('.more').delay(500).animate({
+			opacity: 1
+		}, 1000);
+	}, function() {
+		$(this).find('.more').animate({
+			opacity: 0
+		}, 500);
 	});
 	
 });
+
+
